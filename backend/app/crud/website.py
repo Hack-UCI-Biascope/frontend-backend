38c65a730b7200ce9492e0643a625e2075041896
Ravi Riley <8132955+raviriley@users.noreply.github.com>
2022-02-26 23:45:50 -0800

    ♻️ Add websites to the database if they don't exist

--- backend/app/crud/website.py
+++ backend/app/crud/website.py
@@ -1,7 +1,23 @@
+from typing import Optional
+
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from app import models
+
+
+def add_website(db: Session, website_url: str, bias: Optional[float] = None) -> models.Website:
+    if db.query(models.Website).filter(models.Website.url == website_url).first():
+        raise HTTPException(status_code=400, detail="Website already exists")
+    name = website_url.split("://")[1].split("/")[0].title()
+    if bias:
+        website = models.Website(url=website_url, name=name, avg_bias=bias, num_data_points=1)
+    else:
+        website = models.Website(url=website_url, name=name)
+    db.add(website)
+    db.commit()
+    db.refresh(website)
+    return website
 
 
 def get_website(
@@ -15,12 +31,15 @@ def get_website(
 
 
 def update_website(db: Session, website_url: str, new_bias: float) -> models.Website:
-    website = get_website(db, website_url)
-    num_data_points = website.num_data_points
-    new_avg = (float(website.avg_bias) * float(num_data_points) + new_bias) / (num_data_points + 1)
-    website.avg_bias = float(new_avg)
-    website.num_data_points = num_data_points + 1
-    db.add(website)
-    db.commit()
-    db.refresh(website)
+    website = db.query(models.Website).filter(models.Website.url == website_url).first()
+    if not website:
+        website = add_website(db, website_url, new_bias)
+    else:
+        num_data_points = website.num_data_points
+        new_avg = (float(website.avg_bias) * float(num_data_points) + new_bias) / (num_data_points + 1)
+        website.avg_bias = float(new_avg)
+        website.num_data_points = num_data_points + 1
+        db.add(website)
+        db.commit()
+        db.refresh(website)
     return website
--- backend/app/models/website.py
+++ backend/app/models/website.py
@@ -7,5 +7,6 @@ class Website(Base):
     __tablename__ = "website"
 
     url = Column(String, primary_key=True, index=True, unique=True)
+    name = Column(String)
     avg_bias = Column(Float, default=0.5, nullable=False)
     num_data_points = Column(Integer, default=0, nullable=False)
